e9a8931159da6495f9c5babb8de0402419b480af
Austin Theriot <austinmtheriot@gmail.com>
2020-08-01 16:58:39 -0500

    added GSAP animations to PortfolioCards

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-//GSAP
-// import { gsap } from 'gsap';
-// import { ScrollTrigger } from 'gsap/ScrollTrigger';
-
 //Components
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
@@ -19,8 +15,6 @@ import Sharlat from './routes/Portfolio Articles/Sharlat';
 import LASC from './routes/Portfolio Articles/LASC';
 import JSArt from './routes/Portfolio Articles/JSArt';
 import Email from './routes/Portfolio Articles/Email';
-
-// gsap.registerPlugin(ScrollTrigger);
 
 function App() {
 	return (
--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './ProjectCard.module.css';
 import { Link } from 'react-router-dom';
 
+//GSAP
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
+
 export default (props) => {
+	const sectionRef = useRef(null);
+	useEffect(() => {
+		gsap
+			.timeline({
+				scrollTrigger: {
+					trigger: sectionRef.current,
+					start: 'top 75%',
+					toggleActions: 'play pause play pause',
+				},
+			})
+			.fromTo(
+				sectionRef.current,
+				{
+					xPercent: props.rightalign ? 10 : -10,
+					duration: 1,
+					opacity: 0,
+				},
+				{
+					xPercent: 0,
+					duration: 0.75,
+					opacity: 1,
+				}
+			);
+	}, [props.rightalign]);
 	return (
 		<Link
 			to={props.to || '/'}
@@ -11,6 +40,7 @@ export default (props) => {
 				props.rightalign ? styles.rightalign : null,
 			].join(' ')}>
 			<section
+				ref={sectionRef}
 				className={[
 					styles.section,
 					props.rightalign ? styles.rightalign : null,
