be5671313b381bc3c02dced8ad5b922300973dee
Austin Theriot <austinmtheriot@gmail.com>
2020-08-03 14:32:52 -0500

    added active links to nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,36 +1,79 @@
 import React from 'react';
 import styles from './Header.module.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 export default (props) => {
+	const checkIsActive = (string, ...rest) => {
+		console.log(
+			new URL(window.location.href).pathname === string ? true : false
+		);
+		return new URL(window.location.href).pathname === string ? true : false;
+	};
+
 	return (
 		<header className={styles.header}>
 			<nav className={styles.nav}>
 				<ul className={styles.ul}>
 					<div className={styles.rolloutMenu}>
 						<li className={[styles.li, styles.portfolio].join(' ')}>
-							<Link to='/'>Portfolio</Link>
+							<NavLink
+								to='/'
+								activeClassName={styles.isActive}
+								//forces exact path matching (though not required by Router)
+								isActive={() => checkIsActive('/')}>
+								Portfolio
+							</NavLink>
 						</li>
 						<li className={styles.li}>
-							<Link to='/presto'>Presto</Link>
+							<NavLink
+								to='/presto'
+								activeClassName={styles.isActive}
+								isActive={() => checkIsActive('/presto')}>
+								Presto
+							</NavLink>
 						</li>
 						{/* <li className={styles.li}>
-							<Link to='/sharlat'>Sharlat</Link>
+							<NavLink to='/sharlat' activeClassName={styles.isActive} isActive={() => checkIsActive('/sharlat')}>Sharlat</NavLink>
 						</li> */}
 						<li className={styles.li}>
-							<Link to='/lascabling'>LASCabling</Link>
+							<NavLink
+								to='/lascabling'
+								activeClassName={styles.isActive}
+								isActive={() => checkIsActive('/lascabling')}>
+								LASCabling
+							</NavLink>
 						</li>
 						<li className={styles.li}>
-							<Link to='/jsart'>JSArt</Link>
+							<NavLink
+								to='/jsart'
+								activeClassName={styles.isActive}
+								isActive={() => checkIsActive('/jsart')}>
+								JSArt
+							</NavLink>
 						</li>
 						<li className={styles.li}>
-							<Link to='/email-api'>Email API</Link>
+							<NavLink
+								to='/email-api'
+								activeClassName={styles.isActive}
+								isActive={() => checkIsActive('/email-api')}>
+								Email API
+							</NavLink>
 						</li>
 					</div>
 					<li className={styles.li}>
-						<Link to='/about'>About</Link>
+						<NavLink
+							to='/about'
+							activeClassName={styles.isActive}
+							isActive={() => checkIsActive('/about')}>
+							About
+						</NavLink>
 					</li>
 					<li className={styles.li}>
-						<Link to='/contact'>Contact</Link>
+						<NavLink
+							to='/contact'
+							activeClassName={styles.isActive}
+							isActive={() => checkIsActive('/contact')}>
+							Contact
+						</NavLink>
 					</li>
 				</ul>
 			</nav>
