8eae8c7d491dceb364acade110562a7c54352757
Austin Theriot <austinmtheriot@gmail.com>
2020-08-05 16:37:19 -0500

    encapsulated ProjectCards in liks, ongoing mobile bug when returning to Portfolio from a Project page

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import styles from './ProjectCard.module.css';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 
 //GSAP
 import { gsap } from 'gsap';
@@ -9,23 +9,23 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default (props) => {
 	//ref enables GSAP animations
-	const sectionRef = useRef(null);
+	const linkRef = useRef(null);
 	let history = useHistory();
 
 	useEffect(() => {
 		gsap
 			.timeline({
 				scrollTrigger: {
-					trigger: sectionRef.current,
+					trigger: linkRef.current,
 					start: 'top 75%',
 					toggleActions: 'play pause play pause',
 				},
 			})
 			.fromTo(
-				sectionRef.current,
+				linkRef.current,
 				{
 					xPercent: props.rightalign ? 10 : -10,
-					duration: 1,
+					duration: 0.5,
 					opacity: 0,
 				},
 				{
@@ -37,28 +37,31 @@ export default (props) => {
 	}, [props.rightalign]);
 
 	return (
-		<section
-			id={props.id}
-			//when clicked, replace the current url with the object id and go to the location
-			onClick={(e) => {
-				history.replace(`/#${props.id}`);
-				history.push(`/${props.id}`);
-			}}
-			ref={sectionRef}
+		<Link
+			to={`/${props.id}`}
 			className={[
-				styles.section,
+				styles.Link,
 				props.rightalign ? styles.rightalign : null,
-			].join(' ')}>
-			<img alt='' className={styles.img} src={props.img || ''} />
-			<div
-				className={[
-					styles.topDiv,
-					props.whitetext ? styles.whitetext : null,
-					props.color ? styles[props.color] : null,
-				].join(' ')}>
-				<h3 className={styles.title}>{props.title || 'Title'}</h3>
-				<h4 className={styles.subtitle}>{props.subtitle || 'Subtitle'}</h4>
-			</div>
-		</section>
+			].join(' ')}
+			ref={linkRef}>
+			<section
+				id={props.id}
+				//when clicked, replace the current url with the object id and go to the location
+				onClick={(e) => {
+					history.replace(`/#${props.id}`);
+				}}
+				className={styles.section}>
+				<img alt='' className={styles.img} src={props.img || ''} />
+				<div
+					className={[
+						styles.topDiv,
+						props.whitetext ? styles.whitetext : null,
+						props.color ? styles[props.color] : null,
+					].join(' ')}>
+					<h3 className={styles.title}>{props.title || 'Title'}</h3>
+					<h4 className={styles.subtitle}>{props.subtitle || 'Subtitle'}</h4>
+				</div>
+			</section>
+		</Link>
 	);
 };
