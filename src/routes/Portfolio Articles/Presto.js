8b9e6b578e29768d61f7c17821e5b8fbd20bd972
Austin Theriot <austinmtheriot@gmail.com>
2020-08-15 11:16:28 -0500

    updated Presto description (more community-driven now)

--- src/routes/Portfolio Articles/Presto.js
+++ src/routes/Portfolio Articles/Presto.js
@@ -23,12 +23,18 @@ export default (props) => {
 			</h2>
 			<h3 className={generalStyles.h3}>About</h3>
 			<p className={generalStyles.p}>
-				Designed to reduce the barriers to collaboration by enabling musicians
-				and guests to make public posts based on location. Need a singer? Make a
-				post. Looking for a fill-in trumpet player for a gig this weekend? Write
-				a post. Add some details on your own profile so that others can see what
-				musical services you offer. Then, when you've found a musician that
-				you'd like to work with, message them privately to connect.
+				Designed and developed by musicians for musicians. In today's day and
+				age, meeting local musicians should be easy, but until now, there hasn't
+				been a go-to social media space for musicians to connect with other
+				musicians from their communities. Especially today, in the wake of the
+				COVID-19 pandemic, local communities have become more fragmented than
+				ever before. Presto is designed to provide a space for musical
+				communities to reconnect by enabling guests and musicians to make public
+				posts based on location. Trying to meet a singer? Make a post. Want to
+				talk about the new Kronos Quartet album? Write a post. Tell a little
+				about youself on your own profile so that others can learn more about
+				you. Then, if you find a musician you'd like to talk to individually,
+				message them privately to connect.
 			</p>
 			<div className={generalStyles.center}></div>
 			<div className={[generalStyles.center, generalStyles.flex].join(' ')}>
@@ -50,8 +56,8 @@ export default (props) => {
 			<p className={generalStyles.p}>
 				Enabling local musicians to collaborate and be hired more easily.
 				Intended for performers, composers, arrangers, conductors, soloists,
-				ensembles, bands, and any people from the local community interested in
-				hiring them.
+				ensembles, bands, and any guests interested in hiring them or joining
+				the conversation.
 			</p>
 			<a href={CODE_URL} target='_blank' rel='noopener noreferrer'>
 				<img
