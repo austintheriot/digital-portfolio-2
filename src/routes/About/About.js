39f25078b7793f13dcba16ec3828cde1fe8b4d8c
Austin Theriot <austinmtheriot@gmail.com>
2020-12-02 14:10:04 -0600

    updated About page

--- src/routes/About/About.js
+++ src/routes/About/About.js
@@ -9,6 +9,10 @@ import headshot from '../../assets/images/headshot.jpg';
 
 export default (props) => {
 	window.scrollTo(0, 0);
+	const OPEN_SOURCE_JAVASCRIPT_ALGORITHMS =
+		'https://github.com/trekhleb/javascript-algorithms';
+
+	const AUSTINS_GITHUB = 'https://github.com/austintheriot';
 
 	return (
 		<section className={styles.aboutme}>
@@ -21,20 +25,63 @@ export default (props) => {
 					className={styles.headshot}
 				/>
 				<div className={styles.wrapper}>
-					<h2>front end designer and developer</h2>
+					<h2>front end developer</h2>
+					<p>
+						I am self-taught front end developer based in Austin, TX, with
+						experience developing web sites and web apps using JavaScript
+						(ES6+), TypeScript, React, Redux, CSS3, Sass/Scss, HTML5, Rest APIs,
+						JSON, and Git/GitHub.
+					</p>
+
+					<p>
+						Check out my open-source contributions to the largest JavaScript
+						algorithms & data structures repo on GitHub:{' '}
+						<a
+							href={OPEN_SOURCE_JAVASCRIPT_ALGORITHMS}
+							target='_blank'
+							rel='noopener noreferrer'
+							className={styles.showLink}>
+							{OPEN_SOURCE_JAVASCRIPT_ALGORITHMS}
+						</a>
+						.
+					</p>
+
+					<p>
+						I believe that objective metrics are one of the best tools a
+						developer can use to measure success throughout the development
+						process. Because of this, I use automated testing tools like
+						Cypress, React Testing Library, and Jest, along with the static
+						tests available through TypeScript to ensure my React apps and
+						JavaScript code work 100% to specification. I regularly audit my
+						websites with Google Lighthouse to ensure top notch performance,
+						SEO, best practices, and accessibility. I use Google Analytics to
+						know how many people are using my software and how they’re
+						interacting with it. Why settle for *hoping* your software works,
+						when you can *know* that it does through objective metrics?
+					</p>
+
 					<p>
 						Before I became a software developer, I was an award-winning
 						classical composer. Problem solving and creativity—whether audio,
 						visual or text—have always been two of my greatest strengths. When I
-						first discovered programming during my undergraduate degree, I
-						instantly fell in love, realizing that coding provided a way for me
-						to use my talents in tangible ways for other people. Now I design
-						and develop websites and applications for clients as a freelance web
-						developer. I currently hold a customer satisfaction rating of 100%.{' '}
+						first discovered programming during my my masters degree in music at
+						the University of Texas at Austin, I instantly fell in love,
+						realizing that coding provided a way for me to use my talents in
+						tangible ways for other people. Now I design and develop websites
+						and applications for clients as a freelance web developer.
 					</p>
+
 					<p>
-						If you're interested in hiring me, or just want to say hi, please
-						don't hesitate to drop a line.
+						Check out my GitHub{' '}
+						<a
+							href={AUSTINS_GITHUB}
+							target='_blank'
+							rel='noopener noreferrer'
+							className={styles.showLink}>
+							({AUSTINS_GITHUB})
+						</a>{' '}
+						to see some examples of my work. If you're interested in hiring me,
+						or just want to say hi, please don't hesitate to drop a line.
 					</p>
 					<div className={styles.buttonContainer}>
 						<Link to='/contact' className={styles.Link}>
