3205e7b3efbf054990dc6e12aaf7eb119061e1c7
Austin Theriot <austinmtheriot@gmail.com>
2021-06-27 20:37:32 -0500

    darken loading state, fix animation delay

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -15,26 +15,26 @@ export function Spinner() {
 export function Ghost() {
   return (
     <div className={styles.Ghost}>
-      <div className={[styles.Title, styles.Wave].join(' ')} style={{ animationDelay: '0.2s' }} />
-      <div className={[styles.Subtitle, styles.Wave].join(' ')} style={{ animationDelay: '0.3s' }} />
+      <div className={[styles.Title, styles.Wave].join(' ')} style={{ animationDelay: '-0.3s' }} />
+      <div className={[styles.Subtitle, styles.Wave].join(' ')} style={{ animationDelay: '-0.2s' }} />
       <div className={styles.Header}>
-        <div className={styles.Wave} style={{ animationDelay: '0.4s' }} />
+        <div className={styles.Wave} style={{ animationDelay: '-0.1s' }} />
       </div>
       <div className={styles.Paragraph}>
-        <span className={styles.Wave} style={{ animationDelay: '0s' }} />
-        <span className={styles.Wave} style={{ animationDelay: '0.1s' }} />
-        <span className={styles.Wave} style={{ animationDelay: '0.2s' }} />
-        <span className={styles.Wave} style={{ animationDelay: '0.3s' }} />
-        <span className={styles.Wave} style={{ animationDelay: '0.4s' }} />
-        <span className={styles.Wave} style={{ animationDelay: '0.5s' }} />
-        <span className={styles.Wave} style={{ animationDelay: '0.6s' }} />
-        <span className={styles.Wave} style={{ animationDelay: '0.7s' }} />
-        <span className={styles.Wave} style={{ animationDelay: '0.8s' }} />
-        <span className={styles.Wave} style={{ animationDelay: '0.9s' }} />
-        <span className={styles.Wave} style={{ animationDelay: '0s' }} />
-        <span className={styles.Wave} style={{ animationDelay: '0.1s' }} />
-        <span className={styles.Wave} style={{ animationDelay: '0.2s' }} />
-        <span className={styles.Wave} style={{ animationDelay: '0.3s' }} />
+        <span className={styles.Wave} />
+        <span className={styles.Wave} style={{ animationDelay: '-0.1s' }} />
+        <span className={styles.Wave} style={{ animationDelay: '-0.2s' }} />
+        <span className={styles.Wave} style={{ animationDelay: '-0.3s' }} />
+        <span className={styles.Wave} style={{ animationDelay: '-0.4s' }} />
+        <span className={styles.Wave} style={{ animationDelay: '-0.5s' }} />
+        <span className={styles.Wave} style={{ animationDelay: '-0.6s' }} />
+        <span className={styles.Wave} style={{ animationDelay: '-0.7s' }} />
+        <span className={styles.Wave} style={{ animationDelay: '-0.8s' }} />
+        <span className={styles.Wave} style={{ animationDelay: '-0.9s' }} />
+        <span className={styles.Wave} />
+        <span className={styles.Wave} style={{ animationDelay: '-0.1s' }} />
+        <span className={styles.Wave} style={{ animationDelay: '-0.2s' }} />
+        <span className={styles.Wave} style={{ animationDelay: '-0.3s' }} />
       </div>
     </div>
   );
