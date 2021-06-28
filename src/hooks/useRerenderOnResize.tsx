43db9056a6c615256c4a9d9c2062ae1f027b3428
Austin Theriot <austinmtheriot@gmail.com>
2021-06-27 21:52:59 -0500

    only re-render on width changes

--- src/hooks/useRerenderOnResize.tsx
+++ src/hooks/useRerenderOnResize.tsx
@@ -1,18 +1,26 @@
 import { debounce } from 'lodash';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import useStateIfMounted from './useStateIfMounted';
 
 /**
  * HACKY: unmount and remount animations to get new dimension calculations.
+ * Only do this when the new WIDTH does not equal the prev WIDTH
+ * -- do not re-render when new height does not equal prev height
+ * (this prevents mobile bug, since mobile height adjusts frequently on scroll)
  */
 export default function useRerenderOnResize() {
   const [show, setShow] = useStateIfMounted(true);
+  const prevWidth = useRef(window.innerWidth);
+
   useEffect(() => {
     const toggleShow = () => setShow((bool) => !bool);
     const onResize = debounce(
       () => {
-        toggleShow();
-        setTimeout(toggleShow, 0);
+        if (prevWidth.current !== window.innerWidth) {
+          prevWidth.current = window.innerWidth;
+          toggleShow();
+          setTimeout(toggleShow, 0);
+        }
       },
       500,
       { leading: false },
